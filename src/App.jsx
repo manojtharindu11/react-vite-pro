43af2f5dd1d60e5d49b24e1ee5be0d18cb488363
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-03-24 22:13:03 +0530

    props destructing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import Hello from "./components/Hello";
 function App() {
+  const seatNumbers = [1, 5, 7];
+  const person = {
+    name: "John",
+    message: "hi there",
+    age: 22,
+    emoji: "🙂",
+    seatNumbers: [1, 5, 7],
+  };
   return (
     <div className="App">
-      <Hello name="Mike" message="Hi there," />
-      <Hello name="Tom" message="Hello," />
+      <Hello
+        name="Mike"
+        message="Hi there,"
+        emoji="❤️❤️"
+        seatNumbers={seatNumbers}
+        person={person}
+      />
+      <Hello name="Tom" message="Hello," emoji="🍉🍉" />
     </div>
   );
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,10 +1,15 @@
-function Hello(props) {
-  console.log(props);
-  const { name, message } = props;
+function Hello({ name, message, emoji, seatNumbers, person }) {
+  console.log(person);
   return (
-    <h1>
-      {props.message} {props.name}
-    </h1>
+    <>
+      <h1>
+        {message} {name} {emoji} {seatNumbers}
+      </h1>
+      <p>
+        {person?.name} {person?.message} {person?.emoji} {person?.seatNumbers}{" "}
+        {person?.age}
+      </p>
+    </>
   );
 }
 
